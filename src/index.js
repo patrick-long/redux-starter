69b0a62643514aebdf81324ee0fe9f51443744ee
longpatrick <long.patrick1997@gmail.com>
2021-07-13 10:56:04 -0500

    update updatedperson, but also update person unintentionally

--- src/index.js
+++ src/index.js
@@ -13,7 +13,13 @@ const wrap = type => str => `<${type}>${str}</${type}>`;
 const transform = pipe(trim, toLowerCase, wrap('div'));
 console.log(transform(input));
 
-const person = { name: 'John' };
+const person = { 
+    name: 'John',
+    address: {
+        country: 'USA',
+        city: 'San Francisco'
+    }
+};
 const updatedPerson = { ...person, name: 'Bob' };
-
-console.log(updatedPerson);
+updatedPerson.address.city = 'New York'
+console.log(updatedPerson, person);
