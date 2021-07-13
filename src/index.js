c7051700ab61db414de033a115a76d7f8cf6b00a
longpatrick <long.patrick1997@gmail.com>
2021-07-13 11:44:22 -0500

    use map to replace a specific number in array

--- src/index.js
+++ src/index.js
@@ -40,4 +40,8 @@ const added = [...numbers.slice(0, index), 4, ...numbers.slice(index)];
 const removed = numbers.filter(n => n !== 2);
 console.log(removed);
 
+// Updating 
+const updated = numbers.map(n => n === 2 ? 20 : n );
+console.log(updated);
+
 console.log(added);
