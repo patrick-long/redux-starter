913ad959308303edb781706f686e2f06e3dc864f
longpatrick <long.patrick1997@gmail.com>
2021-07-21 10:15:29 -0500

    add unsubscribe function to log when state changes and declare

--- src/index.js
+++ src/index.js
@@ -66,12 +66,18 @@
 
 import store from './store';
 
+const unsubscribe = store.subscribe(() => {
+    console.log("Store changed!", store.getState());
+});
+
 store.dispatch({
     type: "bugAdded",
     payload: {
         description: "Bug1"
     }
 });
+
+unsubscribe();
 
 store.dispatch({
     type: "bugRemoved",
